779af1dbfd8be847580ff5015baddc0640662c68
Jay Patel <pate0304@algonquinlive.com>
2018-01-25 18:27:04 -0500

    UI Fixed As Requirement.
    XML Attached.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.udacity.sandwichclub;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -12,17 +16,27 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 import org.json.JSONException;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    // Member variables for UI
+    TextView mOrigin, mDescription, mIngredients, mAlsoKnownAs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        //UI references
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mOrigin = findViewById(R.id.origin_tv);
+        mAlsoKnownAs = findViewById(R.id.also_known_tv);
+        mDescription = findViewById(R.id.description_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -50,7 +64,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -63,7 +77,20 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    @SuppressLint("SetTextI18n")
+    private void populateUI(Sandwich sandwich) {
+//        if (sandwich.getAlsoKnownAs().equals("")) {
+//            mOrigin.setVisibility(View.GONE);     /} else {
 
+        List<String> listAlsoKnownAs  = sandwich.getAlsoKnownAs();
+        for(int i=0;i<listAlsoKnownAs.size();i++){
+            mAlsoKnownAs.append(listAlsoKnownAs.get(i)+" ");
+        }
+        mOrigin.setText(sandwich.getPlaceOfOrigin());
+        mDescription.setText(sandwich.getDescription());
+        List<String> listIngredients = sandwich.getIngredients();
+        for (int i = 0; i < listIngredients.size(); i++) {
+            mIngredients.append(listIngredients.get(i) + "\n");
+        }
     }
 }
